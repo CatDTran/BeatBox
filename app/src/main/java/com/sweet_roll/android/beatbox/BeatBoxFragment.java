c4053b325f0dce9a308979370d05f3c5ebb515d3
Cat Tran <trandinhcat@gmail.com>
2016-05-15 11:07:15 -0500

    First commit

---
+++ app/src/main/java/com/sweet_roll/android/beatbox/BeatBoxFragment.java
@@ -0,0 +1,28 @@
+package com.sweet_roll.android.beatbox;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by trand_000 on 5/15/2016.
+ */
+public class BeatBoxFragment extends Fragment {
+    public static BeatBoxFragment newInstance()
+    {
+        return new BeatBoxFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.fragment_beat_box, container, false);
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.fragment_beat_box_recycler_view);
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        return view;
+    }
+}
